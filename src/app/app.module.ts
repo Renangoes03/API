451349227a57a767b2ab75c118a0189091d0386a
PedroHPrieto <renangoessantos9@gmail.com>
2023-04-18 11:27:12 -0300

    Finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -14,7 +14,9 @@ import { DogComponent } from './dog/dog.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
